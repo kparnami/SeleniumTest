9421b195fbc658ab6bcba1db546d8125f396a2df
kparnami <kparnami@deloitte.com>
2018-06-06 16:44:46 +0530

    first commit of my code

---
+++ src/test/java/com/container/test/GoogleTest.java
@@ -0,0 +1,40 @@
+package com.container.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.container.pages.GooglePage;
+
+public class GoogleTest {
+
+    private WebDriver driver;
+    private GooglePage google;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://18.207.242.232:4444/wd/hub"), dc);
+        google = new GooglePage(driver);
+    }
+
+    @Test
+    public void googleTest() throws InterruptedException {
+        google.goTo();
+        google.searchFor("automation");
+        Assert.assertTrue(google.getResults().size() >= 10);
+    }
+    
+    @AfterTest
+    public void tearDown() throws InterruptedException {
+        driver.quit();
+    }    
+
+}

62bdee57acd799340677ca9e5587a6c165b5bdc1
kparnami <kparnami@deloitte.com>
2018-06-11 20:17:09 +0530

    Update GoogleTest.java

--- src/test/java/com/container/test/GoogleTest.java
+++ src/test/java/com/container/test/GoogleTest.java
@@ -21,7 +21,7 @@ public class GoogleTest {
     @BeforeTest
     public void setUp() throws MalformedURLException {
         DesiredCapabilities dc = DesiredCapabilities.chrome();
-        driver = new RemoteWebDriver(new URL("http://54.234.244.72:4444/wd/hub"), dc);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
         google = new GooglePage(driver);
     }
 
